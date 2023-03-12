5cd69e903eb116a707ca1aaccaee08a939790177
Niklas Gabriel <nikgab@gmx.de>
2023-03-12 13:18:08 +0100

    author's name

--- QR Code Extension/Resources/popup.js
+++ QR Code Extension/Resources/popup.js
@@ -1,3 +1,10 @@
+//
+//  AppDelegate.swift
+//  QR Code
+//
+//  Created by Niklas Gabriel on 09.03.23.
+//
+
 // Message empfangen
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("message -> " + "title: " + message.title + ", " + "message: " + message.message);
